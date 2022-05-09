9f535c7c245360892207b68c769b26491fc41cf9
Miguel <mevaristo45@gmail.com>
2022-05-09 10:39:11 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function soma(numero1, numero2){
+    return numero1 + numero2;
+}
+
+function subtracao(numero1, numero2){
+    return numero1 - numero2;
+}
+
+function multiplicacao(numero1, numero2){
+    return numero1 * numero2;
+}
+
+function divisao(numero1, numero2){
+    return numero1 / numero2;
+}
+
+const operacaoSoma = soma(10, 2);
+const operacaoSubtracao = subtracao(10 , 2);
+const operacaoMultiplicacao = multiplicacao(10, 2);
+const operacaoDivisao = divisao(10, 2);
+
+console.log(operacaoSoma);
+console.log(operacaoSubtracao);
+console.log(operacaoMultiplicacao);
+console.log(operacaoDivisao);
